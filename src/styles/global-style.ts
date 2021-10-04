6139c393ad02b18e0adc64c3ea0826a598e780df
kwhong95 <kwhong95@kakao.com>
2021-10-04 02:12:52 -0700

    Create Global Style(Emotion)

---
+++ src/styles/global-style.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+
+export const GlobalStyle = css`
+  html {
+    font-family: 'Noto Sans KR', sans-serif;
+    font-size: 14px;
+  }
+
+  body {
+    overflow-x: hidden;
+    letter-spacing: -.05em;
+    background: #101010;
+  }
+
+  p {
+    line-height: 1.6;
+  }
+`;
